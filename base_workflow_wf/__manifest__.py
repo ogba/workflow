862768cdc74516cb0114a660d82268dde825a262
Mohammed Ogba <ogba.awed@gmail.com>
2024-06-02 18:32:04 +0300

    description

--- base_workflow_wf/__manifest__.py
+++ base_workflow_wf/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': "workflow engine workflow approval SLA , Dynamic approvals Delegation delegate",
+    'name': "odoo workflow engine  approval SLA  Dynamic approvals employee Delegation ",
     'version': '17.0',
     'sequence': 1,
-    'summary': 'workflow engine  workflow Dynamic   approvals Approval Notification Notifier delegate Delegation of Approvals  SLA  approval Engine Notifier Email notifications Dynamic Flexible Workflow  Approval Process Sequential Approvals approval dynamic Process flow',
+    'summary': 'odoo workflow engine  workflow Dynamic   approvals Approval Notification all process in odoo delegate Delegation of Approvals  SLA  approval Engine Notifier Email notifications Dynamic Flexible Workflow  Approval Process Sequential Approvals approval dynamic Process flow',
     'description': """workflow engine to automate all odoo process flow without code need , just do configuration
     you can manage workflow  approvals  Delegation of Approvals  SLA  Email notifications Dynamic Flexible Workflow  Approval Process Sequential Approvals and many other condition
     """,
