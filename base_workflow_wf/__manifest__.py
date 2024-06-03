e4462926f487bb8c49a36aa4c178cff72e6312f9
Mohammed Ogba <ogba.awed@gmail.com>
2024-06-03 11:25:11 +0300

    update the dashboard

--- base_workflow_wf/__manifest__.py
+++ base_workflow_wf/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "odoo workflow engine  approval SLA  Dynamic approvals employee Delegation ",
     'version': '17.0',
     'sequence': 1,
-    'summary': 'odoo workflow engine  workflow Dynamic   approvals Approval Notification all process in odoo delegate Delegation of Approvals  SLA  approval Engine Notifier Email notifications Dynamic Flexible Workflow  Approval Process Sequential Approvals approval dynamic Process flow',
+    'summary': ' workflow , workflow process , odoo workflow engine , workflow approval process , workflow Dynamic , multi-level approval , process in odoo delegate Delegation of Approvals ,  SLA , workflow state , Workflow process ,  process Notifier , Email notifications , Dynamic Flexible Workflow , Approval Process , Sequential Approvals , Process flow , advanced statusbar',
     'description': """workflow engine to automate all odoo process flow without code need , just do configuration
     you can manage workflow  approvals  Delegation of Approvals  SLA  Email notifications Dynamic Flexible Workflow  Approval Process Sequential Approvals and many other condition
     """,
